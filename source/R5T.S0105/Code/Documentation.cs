fb0f64add470cd1a9e97ab059aeab56a416218ca
davidcoats <david@getsafetycone.com>
2023-09-15 14:24:42 -0700

    Initial commit.

---
+++ source/R5T.S0105/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0105
+{
+	/// <summary>
+	/// SignatureString scripts.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.S0105/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.S0105
+{
+    public static class Instances
+    {
+		
+    }
+}
